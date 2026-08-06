28ae19b307a90f7755a2ccbaca2d24e85deab1e7
rafaelsdiniz <rafaeldiniz@unitins.br>
2026-08-06 19:33:33 -0300

    feat(data): persiste o elenco sorteavel no draft

--- src/MmaLegacy.Api/Data/Migrations/20260806223323_ElencoDoDraft.Designer.cs
+++ src/MmaLegacy.Api/Data/Migrations/20260806223323_ElencoDoDraft.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MmaLegacy.Api.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MmaLegacy.Api.Data.Migrations
 {
     [DbContext(typeof(ContextoDoJogo))]
-    partial class ContextoDoJogoModelSnapshot : ModelSnapshot
+    [Migration("20260806223323_ElencoDoDraft")]
+    partial class ElencoDoDraft
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
